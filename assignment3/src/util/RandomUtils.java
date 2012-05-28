59be8e8513393de53429f1699e110b583b626737
Sergio Clemente <sergioclemente@gmail.com>
2012-05-28 16:52:56 -0700

    RandomUtils nit fix

--- assignment3/src/util/RandomUtils.java
+++ assignment3/src/util/RandomUtils.java
@@ -5,15 +5,13 @@ import java.util.Random;
 import edu.washington.cs.cse490h.lib.Utility;
 
 public class RandomUtils {
-	private static Random random = new Random();
-	
 	public static String randomString(int length)
 	{
 		StringBuffer sb = new StringBuffer();
 		
 	    for (int i = 0; i < length; i++)
 	    {
-	    	sb.append((char)random.nextInt(26) + 'a');
+	    	sb.append((char)Utility.getRNG().nextInt(26) + 'a');
 	    }
 	    
 	    return sb.toString();
