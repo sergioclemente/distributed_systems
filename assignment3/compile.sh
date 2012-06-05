10cc7c8e35bf13979ecb30082f4574d02a2f8134
Sergio Clemente <sergioclemente@gmail.com>
2012-06-05 10:16:47 -0700

    updating compile & execute scripts

--- assignment3/compile.sh
+++ assignment3/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-$JAVA_HOME/javac -cp ./jars/plume.jar:./jars/lib.jar:./jars/gson-2.1.jar src/node/facebook/*.java src/node/reliable/*.java src/node/rpc/*.java src/node/twophasecommit/*.java src/settings/*.java src/util/*.java -d classes/
+$JAVA_HOME/javac -Xlint:unchecked -cp ./jars/plume.jar:./jars/lib.jar:./jars/gson-2.1.jar src/node/reliable/*.java src/node/rpc/*.java src/node/rpc/paxos/*.java src/node/storage/*.java src/paxos/*.java src/settings/*.java src/util/*.java -d classes/
 
 exit
--- assignment3/execute.sh
+++ assignment3/execute.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-$JAVA_HOME/java -cp src/:jars/plume.jar:jars/lib.jar:jars/gson-2.1.jar:classes/ edu.washington.cs.cse490h.lib.MessageLayer -s -n node.facebook.FacebookRPCNode 
+$JAVA_HOME/java -cp src/:jars/plume.jar:jars/lib.jar:jars/gson-2.1.jar:classes/ edu.washington.cs.cse490h.lib.MessageLayer -s -n node.rpc.paxos.PaxosNode
 
 exit
